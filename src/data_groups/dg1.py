bba127bc839f61f66b67cb95c7f1a19092c45d3f
A77364 <a77364@alunos.uminho.pt>
2019-04-19 21:55:23 +0100

    ASN1 parser implementation

--- src/data_groups/dg1.py
+++ src/data_groups/dg1.py
@@ -1,63 +1,114 @@
-import json
+import asn1_parser as asn1
 
 class DG1:
-    """Data group 1
-    Data:
-        - family_name
-        - name
-        - date_of_birth
-        - date_of_issue
-        - date_of_expiry
-        - issuing_country
-        - issuing_authority
-        - license_number
-        - categories_of_vehicles
+    """
+    CLasse utilizada para representar um grupo de dados 1.
+    
+    Atributos
+    ---------
+    family_name : str
+        nome da família
+    
+    name : str
+        nome do titular
+    
+    date_of_birth : str
+        data de nascimento
+    
+    date_of_issue : str
+        data de emissão
+    
+    date_of_expiry : str
+        data de validade
+    
+    issuing_country : str
+        país emissor
+    
+    issuing_authority : str
+        autoridade emissora
+    
+    license_number : str
+        número da licensa
+    
+    number_of_entries : int
+        número de categorias de veículos
+    
+    categories_of_vehicles : list(str)
+        lista de categorias de veículos/restrições/condições
+    
+    Métodos
+    -------
+    save(filename)
+        Armazena os dados do DG1 num ficheiro, codificados com ASN1
+
+    load(filename)
+        Carrega os dados do DG1 de um ficheiro, codificado com ASN1
+    
+    __str__()
+        Retorna uma string que apresenta os dados do DG1
     """
     def __init__(self, data):
+        """
+        Parâmetros
+        ----------
+        data : dict
+            dicionário com os nomes das variáveis de instância como chaves
+            e respetivo conteúdo como valor
+        """
         if isinstance(data, str):
-            self.load(data)
-        else:
-            self.family_name = data['family_name']
-            self.name = data['name']
-            self.date_of_birth = data['date_of_birth']
-            self.date_of_issue = data['date_of_issue']
-            self.date_of_expiry = data['date_of_expiry']
-            self.issuing_country = data['issuing_country']
-            self.issuing_authority = data['issuing_authority']
-            self.license_number = data['license_number']
-            self.categories_of_vehicles = data['categories_of_vehicles']
+            data = self.load(data)
+        self.family_name = data['family_name']
+        self.name = data['name']
+        self.date_of_birth = data['date_of_birth']
+        self.date_of_issue = data['date_of_issue']
+        self.date_of_expiry = data['date_of_expiry']
+        self.issuing_country = data['issuing_country']
+        self.issuing_authority = data['issuing_authority']
+        self.license_number = data['license_number']
+        self.number_of_entries = data['number_of_entries']
+        self.categories_of_vehicles = data['categories_of_vehicles']
+
 
     def save(self, filename):
-        """Dumps data group to file"""
+        """Armazena os dados do DG1 num ficheiro, codificados com ASN1.
+        
+        Parâmetros:
+        -----------
+        filename : str
+            nome do ficheiro onde se pretende armazenar os dados
+        """
         with open(filename, 'w+') as fp:
-            payload = [
-                self.family_name,
-                self.name,
-                self.date_of_birth,
-                self.date_of_issue,
-                self.date_of_expiry,
-                self.issuing_country,
-                self.issuing_authority,
-                self.license_number,
-                self.categories_of_vehicles
-            ]
-            # TODO: Cifrar?
-            fp.write(json.dumps(payload))
+            hex_data = asn1.encode(self, './configs/dg1.json')
+            fp.write(hex_data)
+
 
     def load(self, filename):
+        """ Carrega os dados do DG1 de um ficheiro, codificado com ASN1.
+        
+        Parâmetros
+        ----------
+        filename : str
+            nome do ficheiro a partir do qual se pretende obter os dados
+        
+        Retorna
+        -------
+        data : dict
+            dicionário com os nomes das variáveis de instância como chaves
+            e respetivo conteúdo como valor
+        """
         with open(filename, 'r') as fp:
-            # TODO: Decifrar?
-            [self.family_name,\
-             self.name,\
-             self.date_of_birth,\
-             self.date_of_issue,\
-             self.date_of_expiry,\
-             self.issuing_country,\
-             self.issuing_authority,\
-             self.license_number,\
-             self.categories_of_vehicles] = json.loads(fp.read())
+            data = asn1.decode(fp.read(), './configs/dg1.json')
+        return data
+
 
     def __str__(self):
+        """ Retorna uma string que apresenta os dados do DG1
+
+        Retorna
+        -------
+        data : str
+            string com os dados do DG1
+        """
         return ';'.join([self.family_name,\
              self.name,\
              self.date_of_birth,\
@@ -66,12 +117,13 @@ class DG1:
              self.issuing_country,\
              self.issuing_authority,\
              self.license_number,\
-             self.categories_of_vehicles])
+             str(self.number_of_entries),\
+             str(self.categories_of_vehicles)])
 
-DATA = {'family_name': 'family_name', 'name': 'name', 'date_of_birth': '20191011', 'date_of_issue': '20191012', 'date_of_expiry': '20191013', 'issuing_country': 'aaa', 'issuing_authority': 'issuing_authority', 'license_number': 'license_number', 'categories_of_vehicles': 'C1;20000315;20100314;93;<=;8000'}
+
+DATA = {'family_name': 'Smithe Williams', 'name': 'Alexander George Thomas', 'date_of_birth': '19700301', 'date_of_issue': '20020915', 'date_of_expiry': '20070930', 'issuing_country': 'JPN', 'issuing_authority': 'HOKKAIDO PREFECTURAL POLICE ASAHIKAWA AREA SAFETY PUBLIC', 'license_number': 'A290654395164273X', 'number_of_entries': 1, 'categories_of_vehicles': ['C1;20000315;20100314;S01;<=;38303030']}
 dg1 = DG1(DATA)
 dg1.save('dg1.txt')
-dg1.load('dg1.txt')
 
 dg2 = DG1('dg1.txt')
 print(dg2)
